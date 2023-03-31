3abe6a8eb5e479c8cf18ed4f198cc3c54a7bc90c
Filippo Conforti <filippo@commercelayer.io>
2023-03-31 21:15:16 +0200

    Added more characters.

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ puts font.write("DUNGEON")
 include Dungeon
 
 attacker = Character.new(type: "daemon", gold: 10)
-receiver = Character.new(type: "knight", gold: 20)
+receiver = Character.new(type: "dragon", gold: 20)
 
 battle = Battle.new(attacker, receiver)
 winner = battle.resolve
